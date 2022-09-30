6a54fdfabc9638017a2ab71a86196217911ecdbd
Lambo95 <lamberto.martini95@gmail.com>
2022-09-30 17:41:35 +0200

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,87 @@
+const cardArray = [
+  {
+    name: "bsgloria",
+    img: "/assets/media/img1-bs.jpg",
+  },
+  {
+    name: "bsgloria",
+    img: "/assets/media/img1-bs.jpg",
+  },
+  {
+    name: "django",
+    img: "/assets/media/img2-dj.jpg",
+  },
+  {
+    name: "django",
+    img: "/assets/media/img2-dj.jpg",
+  },
+  {
+    name: "grindhouse",
+    img: "/assets/media/img3-gh.jpg",
+  },
+  {
+    name: "grindhouse",
+    img: "/assets/media/img3-gh.jpg",
+  },
+  {
+    name: "hollywood",
+    img: "/assets/media/img4-uoh.jpg",
+  },
+  {
+    name: "hollywood",
+    img: "/assets/media/img4-uoh.jpg",
+  },
+  {
+    name: "jackiebrown",
+    img: "/assets/media/img5-jb.jpg",
+  },
+  {
+    name: "jackiebrown",
+    img: "/assets/media/img5-jb.jpg",
+  },
+  {
+    name: "killbill",
+    img: "/assets/media/img6-kb.jpg",
+  },
+  {
+    name: "killbill",
+    img: "/assets/media/img6-kb.jpg",
+  },
+  {
+    name: "leiene",
+    img: "/assets/media/img7-li.jpg",
+  },
+  {
+    name: "leiene",
+    img: "/assets/media/img7-li.jpg",
+  },
+  {
+    name: "pulpfiction",
+    img: "/assets/media/img8-pf.jpg",
+  },
+  {
+    name: "pulpfiction",
+    img: "/assets/media/img8-pf.jpg",
+  },
+  {
+    name: "hateful",
+    img: "/assets/media/img9-he.jpg",
+  },
+  {
+    name: "hateful",
+    img: "/assets/media/img9-he.jpg",
+  },
+  {
+    name: "dancegif",
+    img: "assets/media/dance.gif",
+  },
+  {
+    name: "dancegif",
+    img: "assets/media/dance.gif",
+  },
+];
+
+const grid = document.querySelector(".grid");
+let cardSelect = [];
+
+//Da rifare entro lunedì
